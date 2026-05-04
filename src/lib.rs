89ad6d67d3c37efd909a0f08f864baa64791f13f
sb2bg <sullivanbognar@gmail.com>
2026-05-04 16:55:50 -0500

    Update version + author

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub mod search;
 pub mod uci;
 pub mod util;
 
-pub const ENGINE_AUTHOR: &str = "Volkrix contributors";
+pub const ENGINE_AUTHOR: &str = "Monty Bognar";
 pub const ENGINE_NAME: &str = "Volkrix";
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const SOURCE_COMMIT: &str = env!("VOLKRIX_SOURCE_COMMIT");
